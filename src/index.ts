3e32b7e8d52e9181a4abe25aed2db8b5e33d312f
Daiver500 <alexei.zenev@gmail.com>
2022-09-26 21:11:17 +0300

    Шаблон

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+require("babel-core/register");
+
+import { renderDOM, registerComponent }  from './core';
+
+import OnboardingPage from './pages/onboarding';
+import LoginPage from './pages/login';
+
+import './app.css';
+
+import Button from './components/button';
+import Link from './components/link';
+import Input from './components/input';
+import Layout from './components/layout';
+
+registerComponent(Button);
+registerComponent(Link);
+registerComponent(Input);
+registerComponent(Layout);
+
+document.addEventListener("DOMContentLoaded", () => {
+  // DEV: Расскоментировать нужно страницу для отображения
+
+  const App = new LoginPage();
+  //const App = new OnboardingPage({
+   //links: [
+     // {to: '#signup', text: 'signup'},
+     // {to: '#login', text: 'login'},
+    //]
+  //});
+
+  renderDOM(App);
+});
